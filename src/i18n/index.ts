e21e3f16b22cc77bb0f373e7f6233c0650bbdd45
Dimitri <dimitri@Dimitris-MacBook-Pro.local>
2026-09-12 23:59:56 +0100

    Fix i18n #6

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import fr from "./fr.json";
 import en from "./en.json";
